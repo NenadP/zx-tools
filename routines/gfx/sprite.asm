2f450d5a94d9d36f43a411f2d77ee431dedb9dbe
NenadP <nenopantic@gmail.com>
2025-02-04 10:48:25 +0000

    Fixed sprite routine endless loop / crashing

--- routines/gfx/sprite.asm
+++ routines/gfx/sprite.asm
@@ -15,10 +15,10 @@ unpack_sprite_header:
 
     ld a, h
     ld (sprite_size_address), a
-    inc hl
-
     ld a, l
     ld (sprite_size_address + 1), a
+
+    inc hl
     inc hl
 
     ld a, h
@@ -35,13 +35,7 @@ draw_sprite:
     ld d, a
     ld a, (sprite_size_address + 1)
     ld e, a
-
     call sprite_y_loop
-
-    ; ld a, (screen_address + 1)
-    ; adc a, 32
-    ; ld (screen_address + 1), a
-
     ret
 sprite_y_loop:
     ld b, e
@@ -49,6 +43,7 @@ sprite_y_loop:
     call sprite_x_loop
     pop bc
     dec b
+    ld e, b
     djnz sprite_y_loop
     ret
 
@@ -82,6 +77,7 @@ sprite_x_loop:
     ld (screen_address + 1), a
 
     dec b
+    ld d, b
     djnz sprite_x_loop
     ret 
 
